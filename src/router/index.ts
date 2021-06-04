170969e2108ac0f7bb26a99b2bf61c18ff98a5f6
guozhao <davidkoojohn@163.com>
2021-06-04 22:39:20 +0800

    add ts page

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,12 +8,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
+    path: '/ts',
+    name: 'TS',
+    component: () => import('../views/TS.vue')
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
