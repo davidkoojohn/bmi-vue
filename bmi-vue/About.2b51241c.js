6ce19db8432c605b28ed2f48e4ed585e90c14ed2
davidkoojohn <davidkoojohn@gmail.com>
2021-06-14 12:36:26 +0000

    Deploying to gh-pages from  @ e6003b7c581ae44fa8dbed0efc36f99538474415 🚀

--- bmi-vue/About.2b51241c.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as o,a as s}from"./vendor.89e47050.js";const e={},n={class:"about"},r=s("h1",null,"This is an about page",-1);e.render=function(s,e){return a(),o("div",n,[r])};export default e;
