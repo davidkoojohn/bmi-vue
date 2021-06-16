65b8b653ae68272281d3be67eab7c326769dfb60
guozhao <davidkoojohn@163.com>
2021-06-16 19:22:43 +0800

    create CalcFormComponent

---
+++ src/components/CalcFormComponent.vue
@@ -0,0 +1,72 @@
+<template>
+  <h2>免费计算你的身体质量指数 (BMI)</h2>
+  <el-form
+    :model="bmiFormData"
+    :rules="bmiFormRules"
+    ref="bmiForm"
+    class="bmi-form"
+  >
+    <el-form-item label="身高" prop="height">
+      <el-input v-model.number="bmiFormData.height"></el-input>
+      <span class="unit">单位: 厘米 cm</span>
+    </el-form-item>
+    <el-form-item label="体重" prop="weight">
+      <el-input v-model.number="bmiFormData.weight"></el-input>
+      <span class="unit">单位: 千克 kg</span>
+    </el-form-item>
+    <el-form-item>
+      <el-button class="submit-bmi" type="primary" @click="submitForm">
+        计算BMI
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script setup>
+import {reactive, ref, unref} from "vue";
+import {ElMessage} from "element-plus";
+
+const bmiForm = ref(null)
+const bmiFormData = reactive({
+  height: '',
+  weight: ''
+})
+
+const bmiFormRules = reactive({
+  height: [
+    { required: true, message: '请输入身高', trigger: 'blur' },
+    { type: 'number', message: '请输入合法的数值', trigger: 'blur' }
+  ],
+  weight: [
+    { required: true, message: '请输入体重', trigger: 'blur' },
+    { type: 'number', message: '请输入合法的数值', trigger: 'blur' }
+  ],
+})
+const submitForm = async () => {
+  const form = unref(bmiForm);
+  if (!form) return
+  try {
+    await form.validate()
+    console.log('success', bmiFormData)
+  } catch (error) {
+    ElMessage.error('请正确填写表单！')
+  }
+}
+</script>
+<style>
+.bmi-form {
+  .el-form-item__content {
+    display: flex;
+  }
+  .unit {
+    white-space: nowrap;
+    margin-left: 20px;
+  }
+
+  .submit-bmi {
+    margin-left: 50px;
+  }
+}
+</style>
+
+
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -4,22 +4,7 @@
       <el-row class="container">
         <el-col :span="12" :xs="24">
           <div class="left">
-            <h2>免费计算你的身体质量指数 (BMI)</h2>
-            <el-form :model="bmiFormData" :rules="bmiFormRules" ref="bmiForm" class="bmi-form">
-              <el-form-item label="身高" prop="height">
-                <el-input v-model.number="bmiFormData.height"></el-input>
-                <span class="unit">单位: 厘米 cm</span>
-              </el-form-item>
-              <el-form-item label="体重" prop="weight">
-                <el-input v-model.number="bmiFormData.weight"></el-input>
-                <span class="unit">单位: 千克 kg</span>
-              </el-form-item>
-              <el-form-item>
-                <el-button class="submit-bmi" type="primary" @click="submitForm">
-                  计算BMI
-                </el-button>
-              </el-form-item>
-            </el-form>
+            <CalcForm />
           </div>
         </el-col>
         <el-col :span="12" :xs="24">
@@ -33,52 +18,21 @@
 </template>
 
 <script>
-import { defineComponent, ref, reactive, unref } from 'vue'
-import { ElMessage } from 'element-plus'
+import { defineComponent, ref } from 'vue'
 import StatusComponent from '../components/StatusComponent.vue'
+import CalcFormComponent from '../components/CalcFormComponent.vue'
 
 export default defineComponent({
   components: {
     Status: StatusComponent,
+    CalcForm: CalcFormComponent,
   },
   setup: (props, context) => {
     const list = ref([])
-    const bmiForm = ref(null)
-    const bmiFormData = reactive({
-      height: '',
-      weight: ''
-    })
-
-    const bmiFormRules = reactive({
-      height: [
-        { required: true, message: '请输入身高', trigger: 'blur' },
-        { type: 'number', message: '请输入合法的数值', trigger: 'blur' }
-      ],
-      weight: [
-        { required: true, message: '请输入体重', trigger: 'blur' },
-        { type: 'number', message: '请输入合法的数值', trigger: 'blur' }
-      ],
-    })
-
     const status = ref('')
 
-    const submitForm = async () => {
-      const form = unref(bmiForm);
-      if (!form) return
-      try {
-        await form.validate()
-        console.log('success', bmiFormData)
-      } catch (error) {
-        ElMessage.error('请正确填写表单！')
-      }
-    }
-
     return {
-      bmiForm,
-      bmiFormData,
-      bmiFormRules,
-      status,
-      submitForm
+      status
     }
   }
 })
@@ -91,26 +45,7 @@ $bg-color: #f00;
   max-width: 1200px;
   width: 100%;
 
-  .left {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 auto;
-
-    .el-form-item__content {
-      display: flex;
-    }
-
-    .unit {
-      white-space: nowrap;
-      margin-left: 20px;
-    }
-
-    .submit-bmi {
-      margin-left: 50px;
-    }
-  }
-
-  .right {
+  .left, .right {
     width: 100%;
     max-width: 400px;
     margin: 0 auto;
